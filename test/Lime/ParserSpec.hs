eeb087e754425400724462b362bf2f3cfcd70e67
Jonathan Chow <jonathan@movio.co>
2015-01-11 15:32:06 +1300

    Refactoring tests in prep for parsing list.

--- test/Lime/ParserSpec.hs
+++ test/Lime/ParserSpec.hs
@@ -13,29 +13,36 @@ instance Eq ParseError where
 spec :: Spec
 spec = do
   describe "lime parser" $ do
-    it "parses numbers" $ property $ \x ->
-      astify (show x) `shouldBe` Right (Number x)
-
-    it "parses strings" $ property $ \x ->
-      astify (escapeString x) `shouldBe` Right (String x)
-
-    it "parses atoms" $ forAll validAtoms $ \x ->
-      astify x `shouldBe` Right (Atom x)
+    it "parses numbers" $ forAll numberGen verifyAst
+    it "parses strings" $ forAll stringGen verifyAst
+    it "parses atoms" $ forAll atomGen verifyAst
 
 ----
 
-escapeString :: String -> String
-escapeString = quote . escape
-  where quote s = "\"" ++ s ++ "\""
+verifyAst :: (String, Expr) -> IO ()
+verifyAst (code, ast) = astify code `shouldBe` Right ast
+
+numberGen :: Gen (String, Expr)
+numberGen = do
+              num <- arbitrary
+              return (show num, Number num)
+
+stringGen :: Gen (String, Expr)
+stringGen = do
+              str <- arbitrary
+              return (escapeStr str, String str)
+  where escapeStr = quote . escape
+        quote s = "\"" ++ s ++ "\""
         escape s = foldr ($) s (zipWith replace find repl)
         find = ["\t",  "\n",  "\"",   "\\"]
         repl = ["\\t", "\\n", "\\\"", "\\\\"]
 
-validAtoms :: Gen String
-validAtoms = do
-               head <- elements letter
-               rest <- listOf $ elements $ letter ++ digit ++ symbol
-               return $ head:rest
+atomGen :: Gen (String, Expr)
+atomGen = do
+            head <- elements letter
+            rest <- listOf $ elements $ letter ++ digit ++ symbol
+            let atom = head : rest
+            return (atom, Atom atom)
   where letter = ['a'..'z'] ++ ['A'..'Z']
         digit = ['0'..'9']
         symbol = "!#$%&|*+-/:<=>?@^_~"
